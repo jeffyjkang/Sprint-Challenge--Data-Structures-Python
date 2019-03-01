295d9e43e33dd5a73cc08701846b3138381156a5
jeffyjkang <jeffyjkang@gmail.com>
2019-03-01 13:37:36 -0800

    update

--- Data_Structures_Answers.md
+++ Data_Structures_Answers.md
@@ -4,23 +4,32 @@ Add your answers to the questions below.
    O(n) each node is visited and a value provided
 
 2. What is the space complexity of your `depth_first_for_each` function?
-   O(n) each node value is stored
+   average O(log n) total number of levels balanced tree, O(n) worst case
 
 3. What is the runtime complexity of your `breadth_first_for_each` method?
    O(n) each node is visited and a value provided
 
 4. What is the space complexity of your `breadth_first_for_each` method?
    O(n) each node value is stored
+   O(2^log n) for balanced tree
 
 5. What is the runtime complexity of the provided code in `names.py`?
-   O(n^2) nested for loop, compares each iteration with a second iteration
+   O(n\*m) n for names in name_1 , m for names in name_2
 
 6. What is the space complexity of the provided code in `names.py`?
-   O(n) compared values with same name are returned and stored
+   O(n \* m) total number of elements
 
 7. What is the runtime complexity of your optimized code in `names.py`?
    O(n) set intersections, will run and compare the two set intersections for
    each value in both sets
 
+   binary search or binary search tree : O (n log n)
+
 8. What is the space complexity of your optimized code in `names.py`?
-   O(n) compared values with same name are returned and stored
+   n would be len of names_1
+   m would be len of names_2
+   O(m + n)
+   O(n) worst case when all names are duplicates
+
+   binary search tree: O (m + n)
+   binary search: O(n)
--- search/binary_search_tree.py
+++ search/binary_search_tree.py
@@ -6,38 +6,49 @@ class BinarySearchTree:
 
     def depth_first_for_each(self, cb):
         # initialize cb input to self.value
+        print(f'before cb: {self.value}')
         cb(self.value)
         # if we are capable of going left
         if self.left:
             # recursively call depth first_ search on left side
+            print(f'self.left.value: {self.left.value}')
             self.left.depth_first_for_each(cb)
         # once we have gone furthest left possible, we check if we can go right
         if self.right:
             # recursively call depth first_ search on right side
+            print(f'self.right.value: {self.right.value}')
             self.right.depth_first_for_each(cb)
 
     def breadth_first_for_each(self, cb):
         # initialize cb input to self.value
+        print(f'before cb: {self.value}')
         cb(self.value)
         # create an empty queue for level traversal, breadth first search is FIFO
         queue = []
+        print(f'queue: {queue}')
         # if we are capable of going left append to queue
         if self.left:
+            print(f'self.left.value: {self.left.value}')
             queue.append(self.left)
         # if we are capable of going right append to queue
         if self.right:
+            print(f'self.right.value: {self.right.value}')
             queue.append(self.right)
         # while the length of queue is greater than 0 we will pop the node's value in the queue
         while len(queue) > 0:
             # assign the current_queue to the value we pop
             current_queue = queue.pop(0)
+            print(f'current_queue: {current_queue.value}')
             # recursively call current_queue value
             cb(current_queue.value)
             # if we are capable of going left from the current queue, append to queue
             if current_queue.left:
+                print(f'current_queue.left.value: {current_queue.left.value}')
                 queue.append(current_queue.left)
             # if we are capable of going right from the current queue, append to queue
             if current_queue.right:
+                print(
+                    f'current_queue.right.value: {current_queue.right.value}')
                 queue.append(current_queue.right)
 
     def insert(self, value):
